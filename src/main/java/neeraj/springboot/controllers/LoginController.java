0d0ccb371636c995aa1069e460fde2b3288132a9
NeerajProfinch-489 <neeraj.patel@profinch.com>
2020-04-30 17:54:58 +0530

    commit

--- src/main/java/neeraj/springboot/controllers/LoginController.java
+++ src/main/java/neeraj/springboot/controllers/LoginController.java
@@ -2,11 +2,14 @@ package neeraj.springboot.controllers;
 
 import neeraj.springboot.models.Login;
 import neeraj.springboot.services.LoginServices;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class LoginController {
     private final LoginServices loginServices;
 
@@ -14,8 +17,15 @@ public class LoginController {
         this.loginServices = loginServices;
     }
 
+    @RequestMapping("/")
+    public String index(){
+        return "index.html";
+    }
+
     @PostMapping("/login")
-    public Login saveLogin(@RequestBody Login login){
-        return loginServices.save(login);
+    public String saveLogin(@RequestParam("username") String username, @RequestParam("password") String password, Model model){
+        Login login = new Login(username,password);
+        model.addAttribute("obj",loginServices.save(login));
+        return "success.html";
     }
 }
